78a3b5c5cb5a21947eee26e601a1511561c0ff75
Marian Morosac <marian.morosac@gmail.com>
2016-11-06 20:06:45 +0200

    First problem solved: acm.tju hello world!

---
+++ source/solved/acm_tju/1001_hello_world.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ *
+ * Created by mgm on 11/6/16.
+ */
+
+public class Main {
+
+    static int n;
+
+    static Scanner fin = new Scanner(System.in);
+    static PrintStream out = null;
+
+    public static void fileInput(String nameOfFile) {
+        try {
+            fin = new Scanner(new File(nameOfFile));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void reading() {
+
+    }
+
+    public static void fileOutput(String nameOfFile) {
+        try {
+            out = new PrintStream(new FileOutputStream(nameOfFile));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.setOut(out);
+    }
+
+    public static void writing() {
+
+    }
+
+    //SOLVE
+    public static void main(String[] args) {
+//        fileInput("input.txt");
+//        fileOutput("output.txt");
+
+        while (fin.hasNextInt()){
+            int c = fin.nextInt();
+            System.out.print((char) c);
+        }
+    }
+}
